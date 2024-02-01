f76b374a6010c521c99f408fee67541302325790
mohdzeeshan2197 <m.zeeshan2197@gmail.com>
2024-02-01 09:50:20 +0530

    first commit

--- README.md
+++ README.md
@@ -1,16 +1,64 @@
-# lab-flask
 
-<!-- ![image](https://user-images.githubusercontent.com/115451707/196919992-edcfea8b-e3f6-4f35-9398-43be66b5622d.png) -->
+# Flask Math Operations Web App
+
+Welcome to the Flask Math Operations Web App! This simple web application allows users to perform basic math operations in a user-friendly environment.
 
 
-To run flask application 
+## Table of Contents
+- Getting Started
 
+- Prerequisites
+
+- Installation
+
+- Usage
+
+- Features
+
+- Contributing
+
+- License
+
+- Acknowledgments
+## Installation
+
+- Clone the repository:
+
+```bash
+https://github.com/mohdzeeshan2197/My_Calculator.git
 ```
-python app.py
-```
+- Navigate to the project directory:
+
+      cd flask-math-operations
+
+- Install dependencies:
+
+      pip install -r requirements.txt
+## Getting Started
+Follow the steps below to set up and run the Flask Math Operations Web App on your local machine.
+## Prerequisites
+- Python 3.x installed
+- Flask library installed (install using pip install Flask)
+## Usage
+
+- Run the Flask application:
+
+- python app.py
+
+- Open your web browser and go to http://127.0.0.1:5000/.
+
+- Enter two numbers and select a math operation to see the result.
+
+## Features
+
+- User-Friendly Interface: Intuitive design for seamless user interaction.
+- Basic Math Operations: Supports addition, subtraction, multiplication, and division.
+- Input Validation: Ensures that users input valid numerical values.
+- Error Handling: Gracefully handles invalid inputs and prevents division by zero.
 
 
-To access your flask application open new tab in and paste the url:
-```
-https://{your_url}.pwskills.app:5000/
-```
+## Acknowledgments
+- Inspiration: This project was inspired by the need for a straightforward math operation web application.
+
+- Flask Community: Thanks to the Flask community for providing a robust and easy-to-use web framework.
+
--- app.py
+++ app.py
@@ -1,10 +1,92 @@
-from flask import Flask
+'''
+from flask import Flask, request,render_template,jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+@app.route('/',methods=['GET','POST'])
+def home_page():
+    return render_template('index.html')
+
+@app.route('/math',methods = ['POST'])
+def math_operation():
+    
+    if request.method == 'POST':
+        op = request.form['operation']
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+    try:
+        if op == 'add':
+            r = num1 + num2
+            result = "The Sum of "+ str(num1) +" and "+ str(num2) +" is "+str(r)
+        
+            return render_template('results.html',result = result)
+
+        if op == 'subtract':
+            r = num1 - num2
+            result = "The Subtraction of "+ str(num1) +" and "+ str(num2) +" is "+str(r)
+        
+            return render_template('results.html',result = result)
+
+        if op == 'multiply':
+            r = num1 * num2
+            result = "The Multiplication of "+ str(num1) +" and "+ str(num2) +" is "+str(r)
+        
+            return render_template('results.html',result = result)
+
+        if op == 'divide':
+            r = num1 / num2
+            result = "The Division of "+ str(num1) +" and "+ str(num2) +" is "+str(r)
+        
+            return render_template('results.html',result = result)
+
+    except Exception as e:
+        er = "ERROR OCCURED :"+str(e)+"\nNOTE: Please Enter Correct Numbers"
+        return render_template('results.html',result = er)
+'''
+
+from flask import Flask, request,render_template,jsonify
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+def home_page():
+    return render_template('index.html')
+
+@app.route('/postman_data',methods = ['POST'])
+def math_operation():
+
+    if request.method == 'POST':
+        op = request.json['operation']
+        num1 = int(request.json['num1'])
+        num2 = int(request.json['num2'])
+    try:
+        if op == 'add':
+            r = num1 + num2
+            result = "The Sum of "+ str(num1) +" and "+ str(num2) +" is "+str(r)
+
+        if op == 'subtract':
+            r = num1 - num2
+            result = "The Subtraction of "+ str(num1) +" and "+ str(num2) +" is "+str(r)
+        
+            
+        if op == 'multiply':
+            r = num1 * num2
+            result = "The Multiplication of "+ str(num1) +" and "+ str(num2) +" is "+str(r)
+        
+            
+        if op == 'divide':
+            r = num1 / num2
+            result = "The Division of "+ str(num1) +" and "+ str(num2) +" is "+str(r)
+        
+        return jsonify(result)
+
+    except Exception as e:
+        er = "ERROR OCCURED : "+str(e)+"NOTE: Please Enter Correct Numbers"
+        return render_template('results.html',result = er)
+
+
+
+
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
